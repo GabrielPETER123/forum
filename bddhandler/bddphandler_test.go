97f5485a0578e22505750a50069bac65097f8669
alexmanfait <alex.manfait@ynov.com>
2025-03-13 11:57:48 +0100

    Corrections test.go et essais avec gorm

--- bddhandler/bddphandler_test.go
+++ bddhandler/bddphandler_test.go
@@ -1,24 +1,23 @@
-package bddphandler_test
+package bddphandler
 
 import (
     "testing"
-    "bddphandler"
     _ "github.com/mattn/go-sqlite3"
 )
 
 func TestInsertUser(t *testing.T) {
-    db, err := bddphandler.InitializeDB(":memory:")
+    db, err :=InitializeDB(":memory:")
     if err != nil {
         t.Fatalf("Could not initialize test database: %v", err)
     }
-    defer bddphandler.CloseDB(db)
+    defer CloseDB(db)
 
-    err = bddphandler.createTables(db)
+    err = createTables(db)
     if err != nil {
         t.Fatalf("Failed to create tables: %v", err)
     }
 
-    _, err = bddphandler.InsertUser(db, "test_pelo", "test@gogole.zaza", "testmdp")
+    _, err = InsertUser(db, "test_pelo", "test@gogole.zaza", "testmdp")
     if err != nil {
         t.Errorf("Failed to insert user: %v", err)
     }
