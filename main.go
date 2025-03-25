e1f808c23d49fd719dc0fc192c5088160232aaea
alexmanfait <alex.manfait@ynov.com>
2025-03-25 11:30:04 +0100

    Ajout d'un main.go pour utiliser la BDD de façon simple

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"bddphandler"
+)
+
+func main() {
+	db, err := bddphandler.InitDB("database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Base de données initialisée avec succès.")
+}
